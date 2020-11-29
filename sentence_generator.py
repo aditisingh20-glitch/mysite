61a63624b28518039edc7ece17d2e1f1ed00dccd
aditisingh20-glitch <73885110+aditisingh20-glitch@users.noreply.github.com>
2020-11-29 17:17:33 +0530

    Add files via upload

---
+++ sentence_generator.py
@@ -0,0 +1,7 @@
+import random
+adjectives=['Green', 'Happy','Hungry', 'Bad', 'Cute']
+nouns=['meow','banana','car','Aditi', 'Jungkook']
+verbs=['brushes','dances', 'is chilling','laughs','draws']
+adverbs=['crazily','happily','meticulously','beautifully','cutely']
+num=random.randrange(0,5)
+print(adjectives[num]+" "+nouns[num]+" "+verbs[num]+" "+adverbs[num]+".")
